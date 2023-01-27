54d0e56b66ea871bb31ecbd3530bf055a372a8e1
Mohammed Mohiuddin <mohiuddinmh@gmail.com>
2023-01-26 22:43:12 -0600

    added css file and useStylesScoped$

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -1,6 +1,7 @@
-import { $, component$, useSignal } from "@builder.io/qwik";
+import { $, component$, useSignal, useStylesScoped$ } from "@builder.io/qwik";
 import Increment from "~/components/counter/increment/increment";
 import Decrement from "~/components/counter/decrement/decrement";
+import styles from './counter.css?inline'
 
 
 interface CounterProps {
@@ -9,6 +10,7 @@ interface CounterProps {
 
 const Counter = component$(({stepSize = 5}: CounterProps) => {
   const counter = useSignal(0)
+  useStylesScoped$(styles)
 
   const increment = $(() => {
     counter.value += stepSize
@@ -20,7 +22,7 @@ const Counter = component$(({stepSize = 5}: CounterProps) => {
 
 
   return <>
-    <h1>{counter.value}</h1>
+    <h1 class='counter'>{counter.value}</h1>
     <Increment increment={increment} />
     <Decrement decrement={decrement} />
   </>
